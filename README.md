e82c5249150fef72b5acc67d395493f436854f6e
luhuijian <luhuijian2013@gmail.com>
2016-02-02 14:40:27 +0800

    add all files

---
+++ README.md
@@ -0,0 +1,16 @@
+# node mock server
+
+![shortshap](./medias/shotshap.png)
+
+## 如何安装
+从npm获取稳定安装包
+
+```
+$ sudo npm install iron-mock
+```
+
+## 使用方法
+
+```
+$ iron-mock server
+```
---
+++ bin/iron-mock.js
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+
+var program = require('commander');
+var nodemon = require('nodemon');
+var pathUtil = require('path');
+var open = require('child_process');
+var log = console.log;
+
+program
+  .version(require('../package.json').version)
+  .command('server')
+  .alias('s')
+  .description('mock server')
+  .option('-p, --port <port>', '自定义端口')
+  .action(function(options) {
+    var port = options.port || 5000;
+    var params = {
+      port: port
+    };
+    var options = [];
+    for(var key in params) {
+      options.push(`--${key} ${params[key]}`)
+    }
+    var appFile = pathUtil.resolve(__dirname, '../lib/app.js');
+
+    open.exec(`node ${appFile} --harmony`);
+
+  })
+  .on('--help', function() {
+    log(' Examples:');
+    log('');
+    log('iron-mock server');
+  });
+
+//默认不传参显示帮助
+if(!process.argv[2]) {
+  program.help();
+  log();
+}
+
+program.parse(process.argv);
